032cf17cf4038d14e78922423470b5f617d90d68
Brent Buchholz <brentbuchholz@gmail.com>
2023-03-23 14:37:25 -0500

    Establish Brand (On This Day)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>One of the MyriaD sites</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
